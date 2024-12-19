ebf267994589129cfa484290da0441155f160ab4
boroznaak23 <borozna_ak23@nuwm.edu.ua>
2024-12-19 14:15:35 +0200

    Initial Project
    Empty project ASP.NET Core MVC;
    Added 1 page to web-site;
    Created Product model;
    Realized transfer Model from Controller to View.

---
+++ Lab2/Controllers/ProductsController.cs
@@ -0,0 +1,27 @@
+﻿using Lab2.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lab2.Controllers
+{
+    public class ProductsController : Controller
+    {
+        [HttpGet]
+        public IActionResult Products()
+        {
+            return View(new ProductModel());
+        }
+
+        [HttpPost]
+        public IActionResult Products(ProductModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                // Дані передаються у вигляд
+                return View(model);
+            }
+
+            // Якщо модель недійсна
+            return View(model);
+        }
+    }
+}
---
+++ Lab2/Models/ProductModel.cs
@@ -0,0 +1,8 @@
+namespace Lab2.Models
+{
+    public class ProductModel
+    {
+        public string? Name { get; set; } // Nullable
+        public int? Memory { get; set; } // Nullable
+    }
+}
