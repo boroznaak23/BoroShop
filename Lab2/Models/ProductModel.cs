40f45bde7b9e131f4d9557416392d7ee6fc71dd5
boroznaak23 <borozna_ak23@nuwm.edu.ua>
2025-01-15 14:13:05 +0200

    Update ProductModel.cs

--- Lab2/Models/ProductModel.cs
+++ Lab2/Models/ProductModel.cs
@@ -3,9 +3,9 @@ namespace Lab2.Models
     public class ProductModel
     {
         public int Id { get; set; }
-        public string Name { get; set; }
+        public string? Name { get; set; }
         public string Model { get; set; }
         public string Category { get; set; }
-        public int Memory { get; set; }
+        public int? Memory { get; set; }
     }
 }
