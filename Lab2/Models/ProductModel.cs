2e36266c586b597536777f5bbc0bcf7e56949dae
boroznaak23 <borozna_ak23@nuwm.edu.ua>
2025-01-15 14:02:52 +0200

    Update ProductModel.cs
    Changed to work with database

--- Lab2/Models/ProductModel.cs
+++ Lab2/Models/ProductModel.cs
@@ -2,7 +2,10 @@ namespace Lab2.Models
 {
     public class ProductModel
     {
-        public string? Name { get; set; } // Nullable
-        public int? Memory { get; set; } // Nullable
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Model { get; set; }
+        public string Category { get; set; }
+        public int Memory { get; set; }
     }
 }
