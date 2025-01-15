dbc58fd27c53c2bfcbbaad0acab0e42388a91e22
boroznaak23 <borozna_ak23@nuwm.edu.ua>
2025-01-15 14:13:13 +0200

    Create ShopContext.cs

---
+++ Lab2/Models/ShopContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Lab2.Models
+{
+    public class ShopContext : DbContext
+    {
+        public DbSet<ProductModel> Products { get; set; }
+
+        public ShopContext(DbContextOptions options) : base(options)
+        {
+
+        }
+    }
+}
